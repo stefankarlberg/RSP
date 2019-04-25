4c6d5b38762afc266506ee7e03a940719e116ea1
stefan_karlberg <stefan.karlberg@smackit.se>
2019-04-25 21:05:16 +0200

    logic added, user can push button and battle against computer. Output in bottom. Test next step

--- src/js/rps.js
+++ src/js/rps.js
@@ -3,3 +3,23 @@ function rps()  {
         return number;
     }
 }
+
+
+if (choice1 === "paper") {
+    if (choice2 === "rock") {
+        return "paper wins";
+    } else {
+        if (choice2 === "scissors") {
+            return "scissors wins";
+        }
+    }
+    if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            return "rock wins";
+        } else {
+            if (choice2 === "paper") {
+                return "scissors wins";
+            }
+        }
+    }
+}
